de7ec90d99cb6f6fb9ba09bdf1c2d66b67aa1fc9
Muhammad Rifky <rifky@virtualspirit.me>
2022-11-23 14:37:24 +0700

    remove ISR from graphql base request

--- apps/with-graph/src/pages/pokemon/[name].tsx
+++ apps/with-graph/src/pages/pokemon/[name].tsx
@@ -1,10 +1,12 @@
+import { useQuery } from "@tanstack/react-query";
 import { useRouter } from "next/router";
 import React from "react";
-import { getPokemon, getPokemons } from "../../services/pokemon";
+import { getPokemon } from "../../services/pokemon";
 
-function PokemonDetail({ pokemon }) {
+function PokemonDetail() {
   const router = useRouter();
-  const sprites = JSON.parse(pokemon.sprites[0].sprites);
+  const { data: pokemon } = usePokemon(router.query.name as string);
+  const sprites = pokemon ? JSON.parse(pokemon.sprites[0].sprites) : {};
   return (
     <div className="relative flex min-h-screen flex-col items-center justify-center p-4">
       <div
@@ -13,77 +15,71 @@ function PokemonDetail({ pokemon }) {
       >
         Back
       </div>
-      <p className="mb-4 text-lg text-red-500">The GraphQL is in BETA state</p>
-      <div className="h-80 w-80 overflow-auto rounded bg-white p-4">
-        <p className="text-xl font-bold uppercase tracking-widest text-sky-500">
-          {pokemon.species[0].name}
-        </p>
-        <p className="text-base font-semibold text-slate-500">
-          Species:{" "}
-          <span className="text-sky-500">{pokemon.species[0].name}</span>
-        </p>
-        <div className="group">
-          <p className="text-base font-semibold text-slate-500">Abilities</p>
-          <div className="">
-            {pokemon.abilities.map((x) => (
-              <p className="ml-4" key={x.name}>
-                {`>`} {x.name}
+      <p className="mb-4 text-lg text-red-500">
+        {pokemon ? "The GraphQL is in BETA state" : "Loading..."}
+      </p>
+      {pokemon && (
+        <>
+          <div className="h-80 w-80 overflow-auto rounded bg-white p-4">
+            <p className="text-xl font-bold uppercase tracking-widest text-sky-500">
+              {pokemon.species[0].name}
+            </p>
+            <p className="text-base font-semibold text-slate-500">
+              Species:{" "}
+              <span className="text-sky-500">{pokemon.species[0].name}</span>
+            </p>
+            <div className="group">
+              <p className="text-base font-semibold text-slate-500">
+                Abilities
               </p>
-            ))}
+              <div className="">
+                {pokemon.abilities.map((x) => (
+                  <p className="ml-4" key={x.name}>
+                    {`>`} {x.name}
+                  </p>
+                ))}
+              </div>
+            </div>
+            <div className="group">
+              <p className="text-base font-semibold text-slate-500">Moves</p>
+              <div className="">
+                {pokemon.moves.map((x) => (
+                  <p className="ml-4" key={x.name}>
+                    {`>`} {x.name}
+                  </p>
+                ))}
+              </div>
+            </div>
           </div>
-        </div>
-        <div className="group">
-          <p className="text-base font-semibold text-slate-500">Moves</p>
-          <div className="">
-            {pokemon.moves.map((x) => (
-              <p className="ml-4" key={x.name}>
-                {`>`} {x.name}
-              </p>
-            ))}
+          <div className="mt-4 flex items-center">
+            {sprites.back_default && (
+              <div className="m-4 grid h-36 w-36 cursor-pointer place-items-center rounded bg-slate-300 hover:bg-white">
+                <img src={sprites.back_default} alt="" />
+              </div>
+            )}
+            {sprites.back_shiny && (
+              <div className="mr-4 grid h-36 w-36 cursor-pointer place-items-center rounded bg-slate-300 hover:bg-white">
+                <img src={sprites.back_shiny} alt="" />
+              </div>
+            )}
+            {sprites.front_default && (
+              <div className="mr-4 grid h-36 w-36 cursor-pointer place-items-center rounded bg-slate-300 hover:bg-white">
+                <img src={sprites.front_default} alt="" />
+              </div>
+            )}
+            {sprites.front_shiny && (
+              <div className="mr-4 grid h-36 w-36 cursor-pointer place-items-center rounded bg-slate-300 hover:bg-white">
+                <img src={sprites.front_shiny} alt="" />
+              </div>
+            )}
           </div>
-        </div>
-      </div>
-      <div className="mt-4 flex items-center">
-        {sprites.back_default && (
-          <div className="m-4 grid h-36 w-36 cursor-pointer place-items-center rounded bg-slate-300 hover:bg-white">
-            <img src={sprites.back_default} alt="" />
-          </div>
-        )}
-        {sprites.back_shiny && (
-          <div className="mr-4 grid h-36 w-36 cursor-pointer place-items-center rounded bg-slate-300 hover:bg-white">
-            <img src={sprites.back_shiny} alt="" />
-          </div>
-        )}
-        {sprites.front_default && (
-          <div className="mr-4 grid h-36 w-36 cursor-pointer place-items-center rounded bg-slate-300 hover:bg-white">
-            <img src={sprites.front_default} alt="" />
-          </div>
-        )}
-        {sprites.front_shiny && (
-          <div className="mr-4 grid h-36 w-36 cursor-pointer place-items-center rounded bg-slate-300 hover:bg-white">
-            <img src={sprites.front_shiny} alt="" />
-          </div>
-        )}
-      </div>
+        </>
+      )}
     </div>
   );
 }
 
-// ISR requirement
-// But I am usually using React-Query to handle APIs
-export async function getStaticProps(ctx) {
-  const pokemon = await getPokemon({ name: ctx.params.name });
-  return {
-    props: { pokemon },
-    revalidate: 60,
-  };
-}
-export async function getStaticPaths() {
-  const pokemons = await getPokemons();
-  const paths = pokemons.gen3_species.map((x) => ({
-    params: { name: x.name },
-  }));
-
-  return { paths, fallback: "blocking" };
-}
+const usePokemon = (name: string) => {
+  return useQuery(["pokemon", name], () => getPokemon({ name }));
+};
 export default PokemonDetail;
